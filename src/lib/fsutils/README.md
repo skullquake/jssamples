2182a58cc854e081a6ae436b8d39aea3b7fe09f6
skullquake <ockert8080@gmail.com>
2021-06-22 12:03:27 -0700

    .

---
+++ src/lib/fsutils/README.md
@@ -0,0 +1,3 @@
+# Description
+
+`fsutils` provides interfaces for common file system operations like reading or creating files for `quickjs`, `goja`, `duktape`, and `mujs`
--- src/observer/src/a.js
+++ src/observer/src/a.js
@@ -1,4 +1,4 @@
-var lib=(function(){
+var liba=(function(){
 	function ObserverList(){
 		this.observerList=[];
 	};
@@ -53,14 +53,66 @@ var lib=(function(){
 	};
 })();
 //--------------------------------------------------------------------------------
-var s=new lib.Subject();
-var o0=new lib.Observer({update:function(val){console.log(["o0",val].join(":"));}});
-var o1=new lib.Observer({update:function(val){console.log(["o1",val].join(":"));}});
-var o2=new lib.Observer({update:function(val){console.log(["o2",val].join(":"));}});
-var o3=new lib.Observer({update:function(val){console.log(["o3",val].join(":"));}});
-s.addObserver(o0);
-s.addObserver(o1);
-s.addObserver(o2);
-s.addObserver(o3);
-s.notify(42);
-s.notify(24);
+{
+	var s=new liba.Subject();
+	var o0=new liba.Observer({update:function(val){console.log(["o0",val].join(":"));}});
+	var o1=new liba.Observer({update:function(val){console.log(["o1",val].join(":"));}});
+	var o2=new liba.Observer({update:function(val){console.log(["o2",val].join(":"));}});
+	var o3=new liba.Observer({update:function(val){console.log(["o3",val].join(":"));}});
+	s.addObserver(o0);
+	s.addObserver(o1);
+	s.addObserver(o2);
+	s.addObserver(o3);
+	s.notify(42);
+	s.notify(24);
+}
+//--------------------------------------------------------------------------------
+var libb=(function(){
+	function Observer(){
+		this.obuf=[];
+	}
+	Observer.prototype={
+		subscribe:function(f){
+			this.obuf.push(f);
+		},
+		unsubscribe:function(f){
+			this.obuf=this.obuf.filter(function(el){
+				if(el!=f)return el;
+			});
+		},
+		notify:function(o,ctx){
+			this.obuf.forEach(function(el){
+				el.call(ctx,o);
+			});
+		}
+	};
+	return{
+		Observer:Observer
+	}
+})();
+//--------------------------------------------------------------------------------
+{
+	var cb0=function(a){console.log("cb0:"+a);};
+	var cb1=function(a){console.log("cb1:"+a);};
+	var cb2=function(a){console.log("cb2:"+a);};
+	var cb3=function(a){console.log("cb3:"+a);};
+	var o=new libb.Observer();
+	o.subscribe(cb0);
+	o.subscribe(cb1);
+	o.subscribe(cb2);
+	o.subscribe(cb3);
+	o.notify("m0");
+	o.unsubscribe(cb3);
+	o.notify("m1");
+	o.unsubscribe(cb2);
+	o.notify("m2");
+	o.unsubscribe(cb1);
+	o.notify("m3");
+	o.unsubscribe(cb0);
+	o.notify("m4");
+	o.notify("m3");
+	o.unsubscribe(cb3);
+	o.notify("m2");
+	o.unsubscribe(cb2);
+	o.notify("m1");
+}
